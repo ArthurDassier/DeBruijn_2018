3d5cbbd6fe165cdf736a6d57ef14e41b5fd48498
Arthur Dassier <arthur.dassier@epitech.eu>
2019-02-22 20:33:05 +0100

    save

--- app/main.hs
+++ app/main.hs
@@ -1,9 +1,19 @@
 import System.Environment
 import System.Exit
 
+myBaro :: String -> String -> Int -> IO()
+myBaro [] [] _ = putStrLn "the fuck ?"
+myBaro a b 0 = putStrLn "OK"
+myBaro a b c = if a == b then putStrLn "KO"
+                else myBaro a (last b : init b) (c - 1)
+
 main = do
     toto <- getArgs
-    if toto == [] then do
+    if toto == ["--check"] then do
+        a <- getLine
+        b <- getLine
+        myBaro a b (length a)
+        else do
             putStrLn "USAGE: ./deBruijn n [a] [--check|--unique|--clean]"
             putStrLn ""
             putStrLn "\t--check\t\tcheck if a sequence is a de Bruijn sequence"
@@ -11,4 +21,3 @@ main = do
             putStrLn "\t--clean\t\tlist cleaning"
             putStrLn "\tn\t\torder of the sequence"
             putStrLn "\ta\t\talphabet [def: “01”]"
-        else putStrLn "OK"
