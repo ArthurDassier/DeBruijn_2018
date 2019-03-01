8bd5ba77d958809d13238690e7e68cee5ebbc662
Arthur Dassier <arthur.dassier@epitech.eu>
2019-03-01 13:25:50 +0100

    Coding Style improved

--- app/Main.hs
+++ app/Main.hs
@@ -28,11 +28,10 @@ data Flag
 flags =
     [Option [] ["check"] (NoArg Check) "check if check",
     Option [] ["clean"] (NoArg Clean) "check if clean",
-    Option [] ["unique"] (NoArg Unique) "check if unique"
-    ]
+    Option [] ["unique"] (NoArg Unique) "check if unique"]
 
 
-printHelp :: IO ()
+printHelp :: IO()
 printHelp = do
     putStrLn "USAGE: ./deBruijn n [a] [--check|--unique|--clean]"
     putStrLn ""
@@ -52,19 +51,32 @@ main = do
             if length args > 2 || length args < 1 then printHelp
                 else if length args == 2 then do
                     preferOne (myFill (rInt (head args)) [(last (last args))]) (rInt (head args)) (last args) 0
+                    exitWith ExitSuccess
                     else do
                         preferOne (myFill (rInt (head args)) "0") (rInt (head args)) "10" 0
+                        exitWith ExitSuccess
         (flags, [], []) -> printHelp
         (flags, args, []) -> do
             if length args > 2 || length args < 1 then printHelp
                 else if length args == 2 then case flags of
-                    [Check] -> putStrLn "OK check"
-                    [Unique] -> putStrLn "OK unique"
-                    [Clean] -> putStrLn "OK clean"
+                    [Check] -> do
+                        putStrLn "OK check"
+                        exitWith ExitSuccess
+                    [Unique] -> do
+                        putStrLn "OK unique"
+                        exitWith ExitSuccess
+                    [Clean] -> do
+                        putStrLn "OK clean"
+                        exitWith ExitSuccess
                     else case flags of
-                            [Check] -> myCheck "10111000" 3 [] 8
+                            [Check] -> do
+                                myCheck "10111000" 3 [] 8
+                                exitWith ExitSuccess
                             [Unique] -> do
                                 a <- getLine
                                 b <- getLine
                                 myUnique a b (length a)
-                            [Clean] -> putStrLn "OK clean"
+                                exitWith ExitSuccess
+                            [Clean] -> do
+                                putStrLn "OK clean"
+                                exitWith ExitSuccess
